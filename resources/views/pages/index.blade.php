049023df52babb4460c5223f7858a55827ed4a88
Dan Mopsick <dmop497@gmail.com>
2017-12-17 14:41:40 -0500

    Implementing nav bar.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,6 +6,19 @@
 
 @section ('content')
   <h1>Hello World!</h1>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br>
+  </br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+</h1>Hello Again</h1>
 @endsection
 
 @section ('js')
---
+++ resources/views/partials/nav_bar.blade.php
@@ -0,0 +1,9 @@
+<!-- Import style for nav bar -->
+<link rel="stylesheet" href="{{ URL::asset('css/nav_bar.css') }}">
+<!-- Import JS for nav bar -->
+<script src="{{ URL::asset('js/nav_bar.js') }}"></script>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <a href="#"><img src="{{ URL::asset('img/dm_logo.png') }}" class="home-logo" /></a>
+  </div>
+</nav>
