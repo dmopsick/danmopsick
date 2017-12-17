38af92762033102dd294bef9eb5608f6fd952fd9
Dan Mopsick <dmop497@gmail.com>
2017-12-16 23:32:57 -0500

    Displaying hello world succesfully.

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+@extends ('templates.page')
+
+@section ('css')
+
+@endsection
+
+@section ('content')
+  <h1>Hello World!</h1>
+@endsection
+
+@section ('js')
+
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="{{ URL::asset('css/footer.css') }}">
+<footer class="footer col-md-12">
+  <p>Copyright  {{ \Carbon\Carbon::now()->year }}  © Dan Mopsick All rights reserved.</p>
+</footer>
